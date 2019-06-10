2b5c9e4bf1a2be7a98ea55a1c7f6ae6fafbc867d
Lokeshreddy <lokeshreddy160@gmail.com>
2019-06-10 18:27:50 +0530

    added comments
    Change-Id: I04ca627c2cbf755d3559a5faa4ae86610becc96b

--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+// planning Add new function
 #include <QMainWindow>
 #include <QtSql>
 #include <QSqlDatabase>
@@ -11,7 +11,7 @@
 namespace Ui {
 class MainWindow;
 }
-
+// planning Add new function
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
